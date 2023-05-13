0015ee1542cf5706fc0fe555e50b644d23fc3b9a
abcd567a <abcd567@hotmail.com>
2023-05-13 15:41:50 -0400

    Update install-ad2.sh

--- install-ad2.sh
+++ install-ad2.sh
@@ -2,6 +2,8 @@
 VERSION=acarsdeco2_rpi2-3_debian9_20181201
 INSTALL_FOLDER=/usr/share/ad2
 
+sudo apt install libudev-dev:armhf  
+
 echo "Creating folder ad2"
 sudo mkdir ${INSTALL_FOLDER}
 #echo "Downloading acarsdeco2 file from Google Drive"
